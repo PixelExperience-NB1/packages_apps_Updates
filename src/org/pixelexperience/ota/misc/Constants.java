fac05521293de8c5bde736c67b27f5aa969558d0
c79 <vexed@riseup.net>
2022-01-06 03:52:59 +0200

    modify OTA

--- src/org/pixelexperience/ota/misc/Constants.java
+++ src/org/pixelexperience/ota/misc/Constants.java
@@ -30,9 +30,13 @@ public final class Constants {
     static final String PROP_AB_DEVICE = "ro.build.ab_update";
     static final String PROP_DEVICE = "org.pixelexperience.device";
     static final String PROP_BUILD_VERSION = "org.pixelexperience.version";
-    static final String OTA_URL = "https://download.pixelexperience.org/ota_v5/%s/%s";
-    static final String OTA_CI_URL = "https://download.pixelexperience.org/ota_ci/%s/%s";
-    static final String MAINTAINER_URL = "https://download.pixelexperience.org/team/%s";
-    static final String DOWNLOAD_WEBPAGE_URL = "https://download.pixelexperience.org/changelog/%s/%s";
+    // return String.format(Constants.OTA_URL, SystemProperties.get(Constants.PROP_DEVICE), SystemProperties.get(Constants.PROP_BUILD_VERSION));
+    // return String.format(Constants.OTA_CI_URL, SystemProperties.get(Constants.PROP_DEVICE), SystemProperties.get(Constants.PROP_BUILD_VERSION));
+    static final String OTA_URL = "https://raw.githubusercontent.com/c79/ignore-this/main/nb1_pe_ota/%s/%s.json";
+    static final String OTA_CI_URL = "https://raw.githubusercontent.com/c79/ignore-this/main/nb1_pe_ota/%s/%s.json";
+    static final String MAINTAINER_URL = "https://github.com/%s";
+    // return String.format(Constants.DOWNLOAD_WEBPAGE_URL, SystemProperties.get(Constants.PROP_DEVICE), fileName);
+    // https://github.com/c79/ignore-this/releases/tag/PE-NB1-20220105-1942.zip <-- example that would work
+    static final String DOWNLOAD_WEBPAGE_URL = "https://github.com/c79/ignore-this/releases/tag/PE-%s-%s";
     static final String EXPORT_PATH = "PixelExperience-Updates/";
 }
